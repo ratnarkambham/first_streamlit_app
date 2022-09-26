02dc8e952212560b23c8bacedd6211f0bc925bb8
ratnakar <114385946+ratnarkambham@users.noreply.github.com>
2022-09-26 14:05:34 +0530

    Update streamlit_app.py
    added breakfast menu header and 3items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
-import pandas
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
-streamlit.dataframe(my_fruit_list)
